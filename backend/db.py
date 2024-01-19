fc53799e467b714183a509805ee8dd76cdda04fe
Vladislav Melnikov <81232295+mindoken@users.noreply.github.com>
2024-01-19 14:48:34 +0300

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -4,12 +4,4 @@ from clickhouse_sqlalchemy import (
     Table, make_session, get_declarative_base, types, engines
 )
 
-uri = 'clickhouse+native://185.43.6.70:9004/mysql_user:fxWwS4IL5dEe'
 
-engine = create_engine(uri)
-session = make_session(engine)
-metadata = MetaData()
-
-Base = get_declarative_base(metadata=metadata)
-
-print(session.query().all())
